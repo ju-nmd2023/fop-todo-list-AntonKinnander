ae8c07f95b1551585ca0371ea06926a9552cd196
fiznjik <149686770+AntonKinnander@users.noreply.github.com>
2024-03-30 18:55:50 +0100

    Making the basic task structure in html

--- main.js
+++ main.js
@@ -3,8 +3,26 @@
 // Define list item structure.
 let listItem = {
   title: "",
+  //date value
   dueDate: 0,
-  category: "",
+  category: "unsorted",
   statusDone: false,
+  color: "blue",
   visible: true,
 };
+
+let divider = {
+  category: "",
+  //Keep at top
+  dueDate: -1,
+  color: "blue",
+  visible: true,
+};
+
+//Create the list only in css - a flex container
+// list items should use flex,
+//Also a divider item, default is ungrouped but users can add their own
+
+//Structure
+// Main container
+// Category divs and item container for that category
