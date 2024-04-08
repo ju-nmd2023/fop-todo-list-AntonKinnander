25fc1b478da54f2af7092c2ca06f828d7f34fb1a
fiznjik <149686770+AntonKinnander@users.noreply.github.com>
2024-04-09 00:31:07 +0200

    distracted forget what ive done but need it on the laptop tmrw

--- main.js
+++ main.js
@@ -3,6 +3,15 @@
 // Define list item structure.
 
 let cards = [];
+const emptyTask = {
+    title: "",
+    //date value
+    dueDate: 0,
+    category: "unsorted",
+    statusDone: false,
+    isDivider: false,
+    visible: true,
+  };
 
 //Create the list only in css - a flex container
 // list items should use flex,
@@ -26,59 +35,45 @@ const taskList = document.getElementById("mainTaskList");
 
 //id tells the program if its a category or a task, default added in main task category
 
-function inputHandler() {
-  let card = {
-    title: "",
-    //date value
-    dueDate: 0,
-    category: "unsorted",
-    statusDone: false,
-    isDivider: false,
-    visible: true,
-  };
-  if (taskInput.value !== "" && taskInput.length <= 150) {
-    card.title = taskInput.value;
-    alert(taskInput.value);
+function processTask(task,new) {
+  if (taskInput.value !== "") {
+    task.title = taskInput.value;
     //Clear taskInput field
     taskInput.value = "";
-  } else if (taskInput.length > 150) {
-    alert("Title can't be over 150 characters");
+    addCard(task.title);
   }
 }
+// function updateLocalstorage(card) {
+//   for (let card of cards) {
+//   }
+// }
+// function getLocalstorage() {}
 
-function updateLocalstorage(card) {
-  for (let card of cards) {
-  }
-}
-function getLocalstorage() {}
-
-function addCard() {
-  if (taskInput.value !== "") {
-    let htmlCard = document.createElement("div");
-    htmlCard.classList.add("card");
-    //Converted to be used as string, create a card with this specific structure, edit variable of title
-    //Want the right card structure with the buttons and everything
-    htmlCard.innerHTML =
-      '      <div class="row">' +
-      '        <input type="checkbox" name="" id="" />' +
-      '        <h1 class="title active">' +
-      card.title +
-      "</h1>" +
-      '        <div class="row r">' +
-      '          <button class="icon date">' +
-      '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
-      "          </button>" +
-      '          <button class="icon edit">' +
-      '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
-      "          </button>" +
-      '          <button class="icon delete">' +
-      '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
-      "          </button>" +
-      "        </div>" +
-      "      </div>";
-    // Clear input
-    taskList.appendChild(card);
-  }
+function addCard(title) {
+  let htmlCard = document.createElement("div");
+  htmlCard.classList.add("card");
+  //Converted to be used as string, create a card with this specific structure, edit variable of title
+  //Want the right card structure with the buttons and everything
+  htmlCard.innerHTML =
+    '      <div class="row">' +
+    '        <input type="checkbox" name="" id="" />' +
+    '        <h1 class="title active">' +
+    title +
+    "</h1>" +
+    '        <div class="row r">' +
+    '          <button class="icon date">' +
+    '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
+    "          </button>" +
+    '          <button class="icon edit">' +
+    '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
+    "          </button>" +
+    '          <button class="icon delete">' +
+    '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
+    "          </button>" +
+    "        </div>" +
+    "      </div>";
+  // Clear input
+  taskList.appendChild(htmlCard);
 }
 
 taskAddBtn.addEventListener("click", inputHandler);
@@ -87,7 +82,7 @@ taskInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     if (!event.shiftKey) {
       event.preventDefault();
-      addCard();
+      inputHandler();
     }
   }
 });
