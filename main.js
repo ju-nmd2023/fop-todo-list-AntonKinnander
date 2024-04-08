8242d650a88a89e11f8858e7161e449491a4051b
AntonKinnander <a.kinnander@icloud.com>
2024-04-08 11:03:43 +0200

    Writing alot of html as astring weird solution but otherwise i need to use jQuery append instead of Node appendChild
    Theres probably a better solution for what i do

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // Define list item structure.
 let listItem = {
-  title: "",
+  title: "Change of value",
   //date value
   dueDate: 0,
   category: "unsorted",
@@ -27,4 +27,43 @@ let divider = {
 // Main container
 // Category divs and item container for that category
 
-document.getElementById("card1").innerHTML("change of plans");
+const taskAddBtn = document.getElementById("newTaskButton");
+const taskInput = document.getElementById("newTaskField");
+const taskList = document.getElementById("mainTaskList");
+
+//Event handler button clicked
+//check if task
+
+function addCard() {
+  if (taskInput.value !== "") {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    //Converted to be used as string, create a card with this specific structure, edit variable of title
+    //Want the right card structure with the buttons and everything
+    card.innerHTML =
+      '      <div class="row">' +
+      '        <input type="checkbox" name="" id="" />' +
+      '        <h1 class="title active">' +
+      taskInput.value +
+      "</h1>" +
+      '        <div class="row r">' +
+      '          <button class="icon date">' +
+      '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
+      "          </button>" +
+      '          <button class="icon edit">' +
+      '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
+      "          </button>" +
+      '          <button class="icon delete">' +
+      '            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"></svg>' +
+      "          </button>" +
+      "        </div>" +
+      "      </div>";
+    // Clear input
+    taskList.appendChild(card);
+    taskInput.value = "";
+  }
+}
+
+taskAddBtn.addEventListener("click", addCard);
+// Thanks: https://stackoverflow.com/questions/14542062/eventlistener-enter-key
+taskInput.addEventListener("submit", addCard);
